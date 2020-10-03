2ffe6d30feb973a7c9026617ec77d0834d4703b0
NickFrangie <NickFrangie@users.noreply.github.com>
2020-10-02 22:32:02 -0400

    Began Random Spawn Programming
    Initial programming for random spawns, general scripts created and obstacle prefabs begun

---
+++ IGDA A-Term Jam/Assets/Scripts/Rock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    /*  Multiple types of rocks with different sprites/behavior
+            Rock1 - single small rock - sprites from the small folder
+            Rock2 - single large rock - sprites from the large folder
+
+
+    */
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
