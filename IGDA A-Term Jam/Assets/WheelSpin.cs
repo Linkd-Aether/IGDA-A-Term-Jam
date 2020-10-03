35ad3e7df767fd9c67e29534cbbffe258f57effb
Jack McEvoy <jackmcevoy15@gmail.com>
2020-10-03 19:31:12 -0400

    Added Moving Wheel BG

--- IGDA A-Term Jam/Assets/WheelSpin.cs
+++ IGDA A-Term Jam/Assets/WheelSpin.cs
@@ -8,32 +8,46 @@ public class WheelSpin : MonoBehaviour
     public Rigidbody2D rb;
 
     private float orientation; //0 - 360 direction of ship
+    private float rotation;
     private float steeringAmount; //value between -1 and 1 used for player input
     private float steeringPower = 1f; //Ships turning ability (higher means capable of sharper turns)
-    private float rotation;
-
-    public float dispRot;
+    private float wheelRotation;
+    private float shipRotation;
+    private Transform bgLeft;
+    private Transform bgCenter;
+    private Transform bgRight;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = this.GetComponent<Rigidbody2D>();
 
+        bgLeft = transform.parent.Find("oceanSect1");
+        bgCenter = transform.parent.Find("oceanSect2");
+        bgRight = transform.parent.Find("oceanSect3");
     }
 
     //Update used for physics calculations as it is independent of frame rate
     void FixedUpdate()
     {
         SetSteeringAmount(-Input.GetAxis("Horizontal")); //Left and right player input
-        //rotation += steeringAmount * steeringPower;
-        //dispRot = rotation;
         orientation = Mathf.Sign(Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up)));
         rotation = rb.rotation + steeringAmount * steeringPower * orientation;
 
-        print(rb.rotation);
 
         rb.SetRotation(rotation);
 
+        moveBG(bgLeft);
+        moveBG(bgCenter);
+        moveBG(bgRight);
+    }
+
+    void moveBG(Transform bg)
+    {
+        bg.localPosition = new Vector3(bg.localPosition.x + steeringAmount / 20, 0);
+        if(bg.localPosition.x > 7.2) bg.localPosition = new Vector3((float)(bg.localPosition.x - 14.4), 0);
+        if (bg.localPosition.x < -7.2) bg.localPosition = new Vector3((float)(bg.localPosition.x + 14.4), 0);
     }
 
     //Sets steeringAmount based on player input (value between -1 and 1)
