2a617e4a88a0c9fbb65b23cdd5e995431ef7aefa
NickFrangie <NickFrangie@users.noreply.github.com>
2020-10-03 06:28:42 -0400

    Moved assets around, groomed code

--- IGDA A-Term Jam/Assets/Scripts/Ship View/Ship.cs
+++ IGDA A-Term Jam/Assets/Scripts/Ship View/Ship.cs
@@ -8,13 +8,12 @@ using UnityEngine.UI;
 public class Ship : MonoBehaviour
 {
     private float speed;
-    private float hullHealth; //Value between 0 (dead) and 100 (full health)
-    //private float wheelPosition; //value between -540 and +540   (allows for 3 full turns)
-    private float sailHeight; //value between 0 (fully lowered) and 1 (fully raised)
-    private float waterLevel; //value between 0 (no water) and 1 (sunk)
+    private float health; // Value between 0 (dead) and 100 (full health)
+    private float sailHeight; // Value between 0 (fully lowered) and 1 (fully raised)
+    private float waterLevel; // Value between 0 (no water) and 100 (sunk)
     private float steeringAmount; //value between -1 and 1 used for player input
-    private float accelerationPower = 0.5f; //Ships acceleration ability (higher means more rapid acceleration)
-    private float steeringPower = 1f; //Ships turning ability (higher means capable of sharper turns)
+    private float accelerationPower = 0.5f; // Ships acceleration ability (higher means more rapid acceleration)
+    private float steeringPower = 1f; // Ships turning ability (higher means capable of sharper turns)
     private float oceanForceMultiplier = 0.4f; //accelerationPower is multiplied by this constant to give a continuous downward force 
                                                       //applied by the ocean's minor waves on the ship
     private float waveForceMultiplier = 0.5f; //dictates force of larger waves pushing ship down
@@ -38,10 +37,10 @@ public class Ship : MonoBehaviour
         };
         sailModels = new Sprite[4]
         {
-            Resources.Load<Sprite>("Assets/Resources/Sprites/Objects/Ship Parts/Sails/sailLarge (2)"), // no damage
-            Resources.Load<Sprite>("Assets/Resources/Sprites/Objects/Ship Parts/Sails/sailLarge (8)"), // partly damaged
-            Resources.Load<Sprite>("Assets/Resources/Sprites/Objects/Ship Parts/Sails/sailLarge (14)"), // mostly damaged
-            Resources.Load<Sprite>("Assets/Resources/Sprites/Objects/Ship Parts/Sails/sailLarge (20)") // destroyed
+            Resources.Load<Sprite>("Sprites/Objects/Ship Parts/Sails/sailLarge (8)"), // no damage
+            Resources.Load<Sprite>("Sprites/Objects/Ship Parts/Sails/sailLarge (2)"), // partly damaged
+            Resources.Load<Sprite>("Sprites/Objects/Ship Parts/Sails/sailLarge (14)"), // mostly damaged
+            Resources.Load<Sprite>("Sprites/Objects/Ship Parts/Sails/sailLarge (20)") // destroyed
         };
 
         SetHealth(100); //Set health to full at start of game
@@ -62,34 +61,33 @@ public class Ship : MonoBehaviour
         rb.AddRelativeForce(-Vector2.right * rb.velocity.magnitude * steeringAmount / 2);
     }
 
-
     // Sets ship health and updates ship damage model (value 0 - 100 here 0 is worst condition and 100 is best)
     public void SetHealth(int value) {
-        hullHealth = Mathf.Clamp(value, 0, 100);
+        health = Mathf.Clamp(value, 0, 100);
         UpdateShipDamageModel();
     }
 
     // Increments ship health by given increment and updates ship damage model
     public void IncrementHealth(int increment) {
-        hullHealth = Mathf.Clamp(hullHealth += increment, 0, 100);
+        health = Mathf.Clamp(health += increment, 0, 100);
         UpdateShipDamageModel();
     }
 
     // Updates ship model based on health and stored hull and sail models
     private void UpdateShipDamageModel() {
-        if (hullHealth >= 80) {
+        if (health >= 80) {
             hullModel.sprite = hullModels[0];
             sailModel.sprite = sailModels[0];
-        } else if (hullHealth >= 60) {
+        } else if (health >= 60) {
             hullModel.sprite = hullModels[0];
             sailModel.sprite = sailModels[1];
-        } else if (hullHealth >= 40) {
+        } else if (health >= 40) {
             hullModel.sprite = hullModels[1];
             sailModel.sprite = sailModels[1];
-        } else if (hullHealth >= 20) {
+        } else if (health >= 20) {
             hullModel.sprite = hullModels[1];
             sailModel.sprite = sailModels[2];
-        } else if (hullHealth >= 0) {
+        } else if (health >= 0) {
             hullModel.sprite = hullModels[2];
             sailModel.sprite = sailModels[2];
         } else {
@@ -98,12 +96,10 @@ public class Ship : MonoBehaviour
         }
     }
 
-    //Sets the ships sail height based on given value (range 0 - 1) where 0 is fully lowered and 1 is fully raised
+    // Sets the ships sail height based on given value (range 0 - 1) where 0 is fully lowered and 1 is fully raised
     public void SetSailHeight(float value) {
         sailHeight = Mathf.Clamp(value, 0f, 1f);
-
         sailModel.transform.localScale = new Vector2(1f, 1.1f - sailHeight);
-
         speed = (1f - sailHeight) * accelerationPower;
     }
 
